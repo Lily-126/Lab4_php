64639a54c8f7ae241fa0a8846271177cb0f0c5be
Lily-126 <lily.le@myokanagan.bc.ca>
2025-10-21 23:05:29 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+
+<!DOCTYPE html>
+<html>
+<head><meta charset="utf-8"><title>Lab Home</title></head>
+<body>
+  <h1>PHP Lab</h1>
+  <p>Current server time: <?= date('Y-m-d H:i:s') ?></p>
+</body>
+</html>
+
